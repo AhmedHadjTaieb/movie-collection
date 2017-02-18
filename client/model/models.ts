96678de947bc693232ccc193aa1d1a4a54caf57f
Ahmed ht <ahmed.ht@gmx.com>
2017-02-18 14:39:23 +0100

    first commit

---
+++ client/model/models.ts
@@ -0,0 +1,6 @@
+export * from './ErrorResponse';
+export * from './GeneralResponse';
+export * from './GetMovieResponse';
+export * from './GetMoviesListResponse';
+export * from './GetMoviesListResponseMovies';
+export * from './Movie';
